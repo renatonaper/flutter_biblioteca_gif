b3c73da6e62fdb71a1b1e3c0640cda278903e0cf
Renato Naper <r.naper@spotpromo.com.br>
2020-08-15 07:44:10 -0300

    tela de buscar gif com textfield

---
+++ android/app/src/main/kotlin/naper/com/fluttergif/MainActivity.kt
@@ -0,0 +1,6 @@
+package naper.com.fluttergif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fluttergif/ui/HomePage.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage() ,
+    theme: ThemeData(
+      hintColor: Colors.white
+    ),
+  ));
+}
+
